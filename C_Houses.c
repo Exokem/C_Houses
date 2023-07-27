06dd7ee0d64a351309e440e68cf9b8da587be325
Exokem <ex010@live.com>
2023-07-27 15:58:10 -0400

    Setup

---
+++ C_Houses.c
@@ -0,0 +1,21 @@
+﻿// SeaHouses.cpp : Defines the entry point for the application.
+//
+
+#include "C_Houses.h"
+
+Uint32* CH_GetSurfacePixel(SDL_Surface* surface, int x, int y)
+{
+	return (Uint8*)surface->pixels + y * surface->pitch + x * surface->format->BytesPerPixel;
+}
+
+Uint32 CH_GetSurfacePixelColor(SDL_Surface* surface, int x, int y)
+{
+	return *(Uint32*)CH_GetSurfacePixel(surface, x, y);
+}
+
+void CH_SetSurfacePixelColor(SDL_Surface* surface, int x, int y, Uint32 color)
+{
+	Uint32* pixel = CH_GetSurfacePixel(surface, x, y);
+	*pixel = color;
+}
+
---
+++ C_Houses.h
@@ -0,0 +1,35 @@
+﻿
+#ifndef _C_HOUSES_
+#define _C_HOUSES_
+
+
+#define _API_ __declspec(dllexport)
+
+#include "SDL.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	/// <summary>
+	/// Provides a pointer to the pixel at a specified position within an SDL_Surface.
+	/// </summary>
+	_API_ Uint32* CH_GetSurfacePixel(SDL_Surface* surface, int x, int y);
+
+	/// <summary>
+	/// Provides the pixel (color) at a specified position within an SDL_Surface.
+	/// <br>
+	/// The returned color is in RGB hex format.
+	/// </summary>
+	_API_ Uint32 CH_GetSurfacePixelColor(SDL_Surface* surface, int x, int y);
+
+	/// <summary>
+	/// Overwrites the pixel at a specified position within an SDL_Surface.
+	/// </summary>
+	_API_ void CH_SetSurfacePixelColor(SDL_Surface* surface, int x, int y, Uint32 color);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
